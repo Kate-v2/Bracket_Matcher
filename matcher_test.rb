0569c5bdbf55995a256dfd3b548e4c2c3e684e49
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-11 09:48:05 -0700

    fix initialize with unintentional nil in test

--- matcher_test.rb
+++ matcher_test.rb
@@ -10,13 +10,13 @@ class SnailTest < Minitest::Test
 
   def setup
     @good   = "[123]{a(b)}"
-    # @bad11  = "[123{a(b)}"
   end
 
 
   def test_it_exists
     assert_instance_of Matcher, Matcher.new(@good)
-    assert_instance_of Matcher, Matcher.new(@bad)
+    bad  = "[123{a(b)}"
+    assert_instance_of Matcher, Matcher.new(bad)
   end
 
   def test_it_knows_when_all_brackets_are_matched
