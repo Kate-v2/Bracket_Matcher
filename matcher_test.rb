013c3e0e8cff8e3b86b37d3e02efe867e165d354
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-11 09:41:58 -0700

    Test spec examples

--- matcher_test.rb
+++ matcher_test.rb
@@ -106,4 +106,23 @@ class SnailTest < Minitest::Test
   end
 
 
+  # ---- Mixed -----
+
+  def test_assignment_spec_examples
+    str1 = '{}';         exp1 = true;  act1 = Matcher.new(str1).assess
+    str2 = '{(';         exp2 = false; act2 = Matcher.new(str2).assess
+    str3 = '{()}';       exp3 = true;  act3 = Matcher.new(str3).assess
+    str4 = '{[)][]}';    exp4 = false; act4 = Matcher.new(str4).assess
+    str5 = ']';          exp5 = false; act5 = Matcher.new(str5).assess
+    str6 = '({[]}{[]})'; exp6 = true;  act6 = Matcher.new(str6).assess
+
+    assert_equal(exp1, act1)
+    assert_equal(exp2, act2)
+    assert_equal(exp3, act3)
+    assert_equal(exp4, act4)
+    assert_equal(exp5, act5)
+    assert_equal(exp6, act6)
+  end
+
+
 end
